c2c670997fc73eee77638f0cd7421f04e2b368d4
матыч <@DESKTOP-30JSL0B>
2024-04-24 12:56:43 +0300

    Добавьте файлы проекта.

---
+++ edit text/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace edit_text
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void color_MouseDown(object sender, MouseButtonEventArgs e) //цвет шрифта пока кнопка
+        {
+
+        }
+
+        private void open_Click(object sender, RoutedEventArgs e)
+        {
+            richtb.Clear();
+            openFileDialog1.FileName = @"data\Text2.txt";
+            openFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+        }
+    }
+}
